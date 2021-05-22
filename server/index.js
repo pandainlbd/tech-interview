207257b1737daffc0213e9e4e21b86a09f4f26a0
Yousef Amar <yousefamar@gmail.com>
2021-05-22 20:29:41 +0100

    add: DB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
-const express = require('express');
+import express from 'express';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 const app = express();
+const mongod = new MongoMemoryServer();
+
+mongod.getInstanceInfo();
 
 app.use(express.static('dist'));
 app.use(express.static('static'));

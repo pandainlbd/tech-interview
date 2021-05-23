5622860ab2a3a9cb060eb630ccfe095434337de3
Yousef Amar <yousefamar@gmail.com>
2021-05-23 10:06:43 +0100

    add: reminder card component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,9 +1,13 @@
 <template>
 	<div>
 		<h1 class="text-3xl text-gray-800 font-bold mb-4">Your reminders for this week:</h1>
-		<ul class="ml-4 list-disc">
-			<li v-for="reminder in reminders">{{ reminder }}</li>
-		</ul>
+		<reminder-card
+			v-for="reminder in reminders"
+			:name="reminder"
+			:key="reminder"
+			:description="reminder"
+			:datetime="new Date()"
+			/>
 		<div>
 			<button class="p-2 my-2 bg-gray-100" @click="openReminderModal">Create</button>
 		</div>
@@ -13,9 +17,11 @@
 
 <script>
 import ReminderModal from './ReminderModal.vue';
+import ReminderCard from './ReminderCard.vue';
 
 export default {
 	components: {
+		ReminderCard,
 		ReminderModal,
 	},
 	data: () => ({
---
+++ src/components/ReminderCard.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<h3>{{ name }}</h3>
+		<p>{{ datetime }}</p>
+		<p>{{ description }}</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		datetime: {
+			type: Date,
+			required: true,
+		},
+	},
+	mounted() {
+	},
+	methods: {
+	}
+}
+</script>
+
+<style scoped>
+</style>

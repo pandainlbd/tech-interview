9b235598423fa8b635b4962754cf74afee56cdf9
Yousef Amar <yousefamar@gmail.com>
2021-05-23 01:56:38 +0100

    add: reminder modal

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,11 +1,33 @@
 <template>
 	<div>
-		Hi there
+		<h1 class="text-3xl text-gray-800 font-bold mb-4">Your reminders for this week:</h1>
+		<ul class="ml-4 list-disc">
+			<li v-for="reminder in reminders">{{ reminder }}</li>
+		</ul>
+		<div>
+			<button class="p-2 my-2 bg-gray-100" @click="openReminderModal">Create</button>
+		</div>
+		<reminder-modal ref="reminderModal" />
 	</div>
 </template>
 
 <script>
+import ReminderModal from './ReminderModal.vue';
+
 export default {
+	components: {
+		ReminderModal,
+	},
+	data: () => ({
+		reminders: ['asda', 'asdasd'],
+	}),
+	mounted() {
+	},
+	methods: {
+		openReminderModal() {
+			this.$refs.reminderModal.open();
+		}
+	}
 }
 </script>
 
---
+++ src/components/ReminderModal.vue
@@ -0,0 +1,40 @@
+<template>
+	<dialog ref="dialog" class="border border-black rounded-md shadow-md">
+		<div class="flex flex-col">
+			<button class="absolute top-0 right-0 m-2 p-2" @click="close">🗙</button>
+			<h2 class="text-2xl text-gray-800 font-bold mb-4">New reminder</h2>
+			<input class="p-2 my-2" type="text" v-model="name" placeholder="name"/>
+			<textarea class="p-2 my-2" type="text" v-model="description" placeholder="description"/>
+			<input class="p-2 my-2" type="datetime-local" v-model="datetime" />
+			<button class="p-2 my-2 bg-gray-100" @click="create">Create</button>
+		</div>
+	</dialog>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		isOpen: true,
+		name: '',
+		description: '',
+		datetime: new Date(),
+	}),
+	mounted() {
+	},
+	methods: {
+		open() {
+			this.$refs.dialog.showModal();
+		},
+		close() {
+			this.$refs.dialog.close();
+			console.log('sad');
+		},
+		create() {
+			console.log(this.name, this.description);
+		},
+	}
+}
+</script>
+
+<style scoped>
+</style>

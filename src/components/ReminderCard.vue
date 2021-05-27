4cbbfc52c6f55e75d980e992874eea5e865c6235
Surya Rajendhran <cielograndesurya@gmail.com>
2021-05-27 09:16:01 +0530

    Add audio

--- src/components/ReminderCard.vue
+++ src/components/ReminderCard.vue
@@ -29,6 +29,26 @@
       <p class="text-xs text-gray-600">{{ datetime }}</p>
       <p>{{ description }}</p>
     </div>
+    <button
+      v-show="audio"
+      @click="toggle"
+      class="bg-gray-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 w-4"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
+        />
+      </svg>
+    </button>
   </div>
 </template>
 

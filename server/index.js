48e8349e9c3d5c52ac59ad2c7f5054b4f07fd61c
Surya Rajendhran <cielograndesurya@gmail.com>
2021-06-07 12:37:22 +0530

    Removed previous commits?

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongodb from 'mongodb';
 const { MongoClient } = mongodb;
 import reminder from './reminder.js';
 
 const app = express();
 // const mongod = new MongoMemoryServer();
-const uri = "mongodb://guest:guest@seed.amar.io:27017/krew?authSource=admin&readPreference=primary&authMechanism=DEFAULT&ssl=false";
+const uri = "mongodb+srv://swoonz:128r7fbr@cluster0.8oblv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 console.log('DB at:', uri);
 const mongoClient = new MongoClient(uri, {
 	useNewUrlParser: true,
@@ -19,17 +18,6 @@ const db = mongoClient.db('tech-interview');
 // Add some fake data
 const remindersCol = db.collection('reminders');
 // remindersCol.drop();
-// remindersCol.insertMany([
-// 	{
-// 		name: 'Do shopping',
-// 		description: 'Remember to buy milk, eggs, and bread.',
-// 		datetime: new Date(+new Date() + 1000 * 60 * 60 * 3),
-// 	}, {
-// 		name: 'Call Bob',
-// 		description: 'Regarding the client meeting.',
-// 		datetime: new Date(+new Date() + 1000 * 60 * 60 * 28),
-// 	},
-// ]);
 
 app.use(express.static('dist'));
 app.use(express.static('static'));
